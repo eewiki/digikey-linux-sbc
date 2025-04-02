8a618412c0e3e0bc5ecb2299e34f241903990cee
Robert Nelson <robertcnelson@gmail.com>
2025-04-02 16:54:31 -0500

    stm32mp157: output to input
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- STMicroelectronics/stm32mp157a-dk1/build_uboot.sh
+++ STMicroelectronics/stm32mp157a-dk1/build_uboot.sh
@@ -3,10 +3,10 @@
 #docker build --no-cache --progress=plain --output=deploy -f Dockerfile.uboot .
 #docker build --progress=plain --output=deploy -f Dockerfile.uboot .
 
-docker build --output=deploy -f Dockerfile.uboot .
+docker build --output=input -f Dockerfile.uboot .
 
 if [ -f /usr/bin/tree ] ; then
-	tree -hD ./deploy/
+	tree -hD ./input/
 fi
 
 #
--- STMicroelectronics/stm32mp157c-dk2/build_tfa.sh
+++ STMicroelectronics/stm32mp157c-dk2/build_tfa.sh
@@ -3,10 +3,10 @@
 #docker build --no-cache --progress=plain --output=deploy -f Dockerfile.uboot .
 #docker build --progress=plain --output=deploy -f Dockerfile.uboot .
 
-docker build --output=deploy -f Dockerfile.tfa .
+docker build --output=input -f Dockerfile.tfa .
 
 if [ -f /usr/bin/tree ] ; then
-	tree -hD ./deploy/
+	tree -hD ./input/
 fi
 
 #
