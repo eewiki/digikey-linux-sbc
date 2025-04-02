fa34feb56feb4c68755996435790d86fc6de4f7c
Robert Nelson <robertcnelson@gmail.com>
2025-04-02 14:56:28 -0500

    ci: cleanup apt DEBIAN_FRONTEND
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- STMicroelectronics/stm32mp157a-dk1/build_tfa.sh
+++ STMicroelectronics/stm32mp157a-dk1/build_tfa.sh
@@ -1,8 +1,12 @@
-
 #!/bin/bash
 
 #docker build --no-cache --progress=plain --output=deploy -f Dockerfile.uboot .
 #docker build --progress=plain --output=deploy -f Dockerfile.uboot .
 
-docker build --progress=plain --output=deploy -f Dockerfile.tfa .
-tree -hD ./deploy/
+docker build --output=deploy -f Dockerfile.tfa .
+
+if [ -f /usr/bin/tree ] ; then
+	tree -hD ./deploy/
+fi
+
+#
--- STMicroelectronics/stm32mp157a-dk1/build_uboot.sh
+++ STMicroelectronics/stm32mp157a-dk1/build_uboot.sh
@@ -1,8 +1,12 @@
-
 #!/bin/bash
 
 #docker build --no-cache --progress=plain --output=deploy -f Dockerfile.uboot .
 #docker build --progress=plain --output=deploy -f Dockerfile.uboot .
 
 docker build --output=deploy -f Dockerfile.uboot .
-tree -hD ./deploy/
+
+if [ -f /usr/bin/tree ] ; then
+	tree -hD ./deploy/
+fi
+
+#
