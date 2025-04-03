e3fb72a551011f2c075fc704d22750d7948f8687
Robert Nelson <robertcnelson@gmail.com>
2025-04-03 17:48:03 -0500

    stm32mp157a-dk1: add optee/tfa support
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- STMicroelectronics/stm32mp157a-dk1/build_bootloader.sh
+++ STMicroelectronics/stm32mp157a-dk1/build_bootloader.sh
@@ -4,7 +4,7 @@ if [ -d ./input/ ] ; then
 	sudo rm -rf ./input/ || true
 fi
 
-wfile="Dockerfile.uboot"
+wfile="Dockerfile.bootloader"
 
 #docker build --no-cache --progress=plain --output=input -f ${wfile} .
 #docker build --progress=plain --output=input -f ${wfile} .
--- STMicroelectronics/stm32mp157c-dk2/build_bootloader.sh
+++ STMicroelectronics/stm32mp157c-dk2/build_bootloader.sh
@@ -7,9 +7,9 @@ fi
 wfile="Dockerfile.bootloader"
 
 #docker build --no-cache --progress=plain --output=input -f ${wfile} .
-docker build --progress=plain --output=input -f ${wfile} .
+#docker build --progress=plain --output=input -f ${wfile} .
 
-#docker build --output=input -f ${wfile} .
+docker build --output=input -f ${wfile} .
 
 if [ -f /usr/bin/tree ] ; then
 	tree -hD ./input/
