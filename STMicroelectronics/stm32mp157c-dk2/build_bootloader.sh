288b66669172c565a3698f91a1a99ec652ec2a2a
Robert Nelson <robertcnelson@gmail.com>
2025-04-03 17:42:29 -0500

    stm32mp157c-dk2: add optee/tfa support
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- STMicroelectronics/stm32mp157c-dk2/build_bootloader.sh
+++ STMicroelectronics/stm32mp157c-dk2/build_bootloader.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wfile="Dockerfile.tfa"
+if [ -d ./input/ ] ; then
+	sudo rm -rf ./input/ || true
+fi
+
+wfile="Dockerfile.bootloader"
 
 #docker build --no-cache --progress=plain --output=input -f ${wfile} .
 docker build --progress=plain --output=input -f ${wfile} .
